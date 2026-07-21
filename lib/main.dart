b77cd5b1a0232a6cc9a22c0a8a8b1bdc10ffaaf0
zvespinoza <zvespinoza@istlatroncal.edu.ec>
2026-07-20 20:06:33 -0500

    agregacion ListWiew

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,34 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Text("Directorio IST La Troncal"),
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.amber,
+        ),
+        body: ListView(
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text("Ing: Israel Zurita"),
+              subtitle: Text("Desarrollo Aplicaciones Web"),
+              trailing: Icon(Icons.star, color: const Color.fromARGB(255, 163, 208, 245)),
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text("Ing: Angel Novillo"),
+              subtitle: Text("Desarrollo Aplicaciones Web"),
+              trailing: Icon(Icons.star, color: const Color.fromARGB(255, 163, 208, 245)),
+            ),
+            ListTile(
+              leading: Icon(Icons.person, color: Colors.blue),
+              title: Text("Ing: Cecilia Naula"),
+              subtitle: Text("Desarrollo Aplicaciones Web"),
+              trailing: Icon(Icons.star, color: const Color.fromARGB(255, 163, 208, 245)),
+            ),
+          ],
         ),
       ),
     );
